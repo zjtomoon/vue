0e8f9ac562b920943b4a4deebf906d22cd8e3736
zjtomoon <zjtomoon@outlook.com>
2022-04-10 17:13:19 +0800

    default commit

---
+++ vue2/hello/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div >
+      <h1>文件上传</h1>
+      <FileUpload/>
+    </div>
+</template>
+
+<script>
+    import FileUpload from "./FileUpload";
+    export default {
+        name: "Home",
+      components: {FileUpload}
+    }
+</script>
+
+<style scoped>
+
+</style>
